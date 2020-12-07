42801e2c610260f8abe76ccc702cb9e1a8448082
PhunkyBob <github.com@e-courrier.eu>
2020-12-07 12:16:51 +0100

    Optim.

--- 2020/day_02_A.py
+++ 2020/day_02_A.py
@@ -5,12 +5,8 @@ lines = file.readlines()
   
 count_ok = 0
 for line in lines: 
-    my_matches = re.match(r"(\d+)-(\d+) (\w): (\w+)", line)
-    min = int(my_matches[1])
-    max = int(my_matches[2])
-    char = my_matches[3]
-    pwd = my_matches[4]
-    if min <= pwd.count(char) <= max:
+    min, max, char, pwd = re.match(r"(\d+)-(\d+) (\w): (\w+)", line).groups()
+    if int(min) <= pwd.count(char) <= int(max):
         count_ok = count_ok + 1
 
 print(f"{count_ok} passwords are OK")
--- 2020/day_02_B.py
+++ 2020/day_02_B.py
@@ -5,12 +5,8 @@ lines = file.readlines()
   
 count_ok = 0
 for line in lines: 
-    my_matches = re.match(r"(\d+)-(\d+) (\w): (\w+)", line)
-    pos1 = int(my_matches[1])
-    pos2 = int(my_matches[2])
-    char = my_matches[3]
-    pwd = my_matches[4]
-    if (pwd[pos1 - 1] == char) ^ (pwd[pos2 - 1] == char):
+    pos1, pos2, char, pwd = re.match(r"(\d+)-(\d+) (\w): (\w+)", line).groups()
+    if (pwd[int(pos1) - 1] == char) ^ (pwd[int(pos2) - 1] == char):
         count_ok = count_ok + 1
 
 print(f"{count_ok} passwords are OK")
