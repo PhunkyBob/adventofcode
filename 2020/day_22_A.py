17c3bc3cdfa7c0676d06a8ace2386bdd195ae30b
PhunkyBob <github.com@e-courrier.eu>
2020-12-22 17:35:56 +0100

    Better style.

--- 2020/day_22_A.py
+++ 2020/day_22_A.py
@@ -1,39 +1,44 @@
 # https://adventofcode.com/2020/day/22
 
-# player1, player2 = open("day_22_input_sample.txt").read().split("\n\n")
-player1, player2 = open("day_22_input.txt").read().split("\n\n")
+import time
 
-player1 = list(int(e) for e in player1.split("\n")[1:])
-player2 = list(int(e) for e in player2.split("\n")[1:])
+# input = open("day_22_input_sample.txt").read().split("\n\n")
+input = open("day_22_input.txt").read().split("\n\n")
 
-# Part One
-print("Part One")
-round = 1
-while len(player1) and len(player2):
-    print(f"-- Round {round} --")
+
+player1, player2 = [list(map(int, v)) for v in [player.split("\n")[1:] for player in input]]
+
+def part_one(player1, player2):
+    round = 0
+    while player1 and player2:
+        round += 1
+        # print(f"-- Round {round} --")
+        # print(f"Player 1's deck: {str(player1)}")
+        # print(f"Player 2's deck: {str(player2)}")
+        card1, card2 = player1.pop(0), player2.pop(0)
+        # print(f"Player 1 plays: {card1}")
+        # print(f"Player 2 plays: {card2}")
+        if card1 > card2:
+            # print("Player 1 wins the round!")
+            player1 += [card1, card2]
+        if card1 < card2:
+            # print("Player 2 wins the round!")
+            player2 += [card2, card1]
+        print()
+        
+    print(f"== Post-game results after {round} rounds ==")
     print(f"Player 1's deck: {str(player1)}")
     print(f"Player 2's deck: {str(player2)}")
-    card1 = player1.pop(0)
-    card2 = player2.pop(0)
-    print(f"Player 1 plays: {card1}")
-    print(f"Player 2 plays: {card2}")
-    if card1 > card2:
-        print("Player 1 wins the round!")
-        player1.append(card1)
-        player1.append(card2)
-    if card1 < card2:
-        print("Player 2 wins the round!")
-        player2.append(card2)
-        player2.append(card1)
-    print()
-    round += 1
+    score1 = sum((i + 1) * val for i, val in enumerate(reversed(player1)))
+    score2 = sum((i + 1) * val for i, val in enumerate(reversed(player2)))
+    
+    print(f"Player 1 scores {score1}")
+    print(f"Player 2 scores {score2}")
+    return score1 + score2
 
-print("== Post-game results ==")
-print(f"Player 1's deck: {str(player1)}")
-print(f"Player 2's deck: {str(player2)}")
 
-score1 = sum((i + 1) * val for i, val in enumerate(reversed(player1)))
-score2 = sum((i + 1) * val for i, val in enumerate(reversed(player2)))
-
-print(f"Player 1 scores {score1}")
-print(f"Player 2 scores {score2}")
+# Part One
+start_time = time.time()
+score = part_one(player1, player2)
+print(f"Part One: {score}")
+print("--- %.2f seconds ---" % (time.time() - start_time))
