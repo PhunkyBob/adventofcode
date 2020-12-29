acf8d43283022e35a76611c092feb840f7cbfddd
PhunkyBob <github.com@e-courrier.eu>
2020-12-29 12:11:34 +0100

    2020, Day 23, Part Two

--- 2020/day_23_AB.py
+++ 2020/day_23_AB.py
@@ -1,7 +1,10 @@
 # https://adventofcode.com/2020/day/23
+import time
+from rich.progress import Progress
 
-input_txt = "389125467"
-# input_txt = "247819356"
+
+# input_txt = "389125467"
+input_txt = "247819356"
 
 """Brute copy of lists.
 """
@@ -26,56 +29,100 @@ def move_v1(input):
 
 """ We use only 1 list and work on it.
 """
-def compute(input_txt, loops=100):
-    cup_circle = list(int(e) for e in input_txt)
-    cup_size = len(cup_circle)
-    current_pos = 0
-    pop_cnt = 3
-    for _ in range(loops):
-        current_elem = cup_circle[current_pos]
-        popped = [cup_circle[(current_pos + i) % cup_size] for i in range(1, pop_cnt + 1)]
-        for p in popped:
-            cup_circle.remove(p)
-        destination = current_elem - 1
-        while destination in popped or destination == 0:
-            destination = cup_size if destination == 0 else (destination - 1) % cup_size
+def part_1(cup_circle, loops=100):
+    # with Progress() as progress:
+    #     task1 = progress.add_task("[green]Playing...", total=loops)
+        cup_size = len(cup_circle)
+        current_pos = 0
+        pop_cnt = 3
+
+        for _ in range(loops):
+            current_elem = cup_circle[current_pos]
+            popped = [cup_circle[(current_pos + i) % cup_size] for i in range(1, pop_cnt + 1)]
+            for p in popped:
+                cup_circle.remove(p)
+            destination = current_elem - 1
+            while destination in popped or destination == 0:
+                destination = cup_size if destination == 0 else (destination - 1) % cup_size
+                
+            destination_pos = cup_circle.index(destination)
+            for p in reversed(popped):
+                cup_circle.insert(destination_pos + 1, p)
+            current_pos = (cup_circle.index(current_elem) + 1) % cup_size
+            # progress.update(task1, advance=1)
+        # progress.completed = True
+        return cup_circle
+
+
+""" We work with pointers.
+"""
+def part_2(cups, fill=1_000_000, loops=10_000_000):
+    # with Progress() as progress:
+    #     update_every = 500_000
+    #     task1 = progress.add_task("Setup...", total=fill)
+    #     task2 = progress.add_task("[green]Playing...", total=loops//update_every)
+
+        cup_next = {}
+
+        for i in range(fill):
+            if i < len(cups) - 1:
+                cup_next[cups[i]] = cups[i + 1]
+            elif i == len(cups) - 1:
+                cup_next[cups[-1]] = max(cups) + 1
+            else: 
+                cup_next[i + 1] = (i + 2)
+            # progress.update(task1, advance=1)
             
-        destination_pos = cup_circle.index(destination)
-        for p in reversed(popped):
-            cup_circle.insert(destination_pos + 1, p)
-        current_pos = (cup_circle.index(current_elem) + 1) % cup_size
+        cup_next[fill] = cups[0]
 
-    return cup_circle
+        current_elem = cups[0]
 
+        for i in range(loops):
+            # Short circuit
+            pop_1 = cup_next[current_elem]
+            pop_2 = cup_next[pop_1]
+            pop_3 = cup_next[pop_2]
+            cup_next[current_elem] = cup_next[pop_3]
+            destination =  current_elem - 1
+
+            while destination in [pop_1, pop_2, pop_3] or destination == 0:
+                destination = fill if destination == 0 else destination - 1
+
+            cup_next[pop_3] = cup_next[destination]
+            cup_next[destination] = pop_1
+            current_elem = cup_next[current_elem]
+            # if i % update_every == 0:
+            #     progress.update(task2, advance=1)
+
+        # progress.completed = True
+        return cup_next
 
 
 # Part One
-# input = list(int(e) for e in input_txt)
-
-# total_rounds = 100
-# for i in range(total_rounds):
-#     print(f"-- move {i+1} --")
-#     input = move_v1(input)
-# current = input[0]
-# print("final: " + " ".join(map(lambda x: f"({x})" if x == current else str(x), input)))
-# part_one = input[input.index(1) + 1:] + input[:input.index(1)]
-# print("Part One: ", "".join(map(str, part_one)))
-
-final = compute(input_txt)
+start_time = time.time()
+cup_circle = list(int(e) for e in input_txt)
+final = part_1(cup_circle)
 answer = final[final.index(1) + 1:] + final[:final.index(1)]
 print("Part One: ", "".join(map(str, answer)))
-
+print("--- %.2f seconds ---" % (time.time() - start_time))
 
 
 
 # Part Two
-# input = list(int(e) for e in input_txt)
+# Using same method as Part One, it will take days.
+# start_time = time.time()
+# cup_circle = list(int(e) for e in input_txt)
 # total_cups = 1_000_000
-# input += list(i for i in range(max(input) + 1, total_cups + 1))
+# cup_circle += list(i for i in range(max(cup_circle) + 1, total_cups + 1))
+# final = part_1(cup_circle, loops=10_000_000)
+# index = final.index(1)
+# answer = final[index + 1] * final[index + 2]
+# print(f"Part Two: ", answer)
+# print("--- %.2f seconds ---" % (time.time() - start_time))
 
-# total_rounds = 10_000_00
-# for i in range(total_rounds):
-#     input = move(input)
-# index_1 = input.index(1)
-# part_two = input[index_1 + 1] * input[index_1 + 2]
-# print(f"Part Two: {part_two}")
+start_time = time.time()
+cups = list(int(e) for e in input_txt)
+final = part_2(cups)
+answer = final[1] * final[final[1]]
+print(f"Part Two: ", answer)
+print("--- %.2f seconds ---" % (time.time() - start_time))
