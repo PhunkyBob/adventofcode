e7643d86917b4ecc9414d92994ed73ae78b595f9
PhunkyBob <github.com@e-courrier.eu>
2021-12-02 10:12:42 +0100

    Factorisation du code

--- 2021/2021_day_01_AB.py
+++ 2021/2021_day_01_AB.py
@@ -3,31 +3,34 @@
 
 import time
 
-def process(input):
+def solve_part_one(input):
     return sum([1 if input[i] > input[i-1] else 0 for i in range(1, len(input))])
+
+def solve_part_one_and_two(input, window_size=1):
+    return sum([1 if sum(input[i-window_size+1:i+1]) > sum(input[i-window_size:i]) else 0 for i in range(window_size, len(input))])
 
 
 if __name__ == '__main__':
     start_time = time.time()
 
-    input_file = '2021_day_01_input_sample.txt'
+    # input_file = '2021_day_01_input_sample.txt'
     input_file = '2021_day_01_input.txt'
     input = [int(x) for x in open(input_file, 'r').readlines()]
 
     """Part One"""
-    result = process(input)
+    # result = solve_part_one(input)
+    result = solve_part_one_and_two(input)
     print(f'Day 01 Part One: {result}')
 
     """Part Two"""
-    window_size = 3
+    
 
     # # Version avec recyclage de la fonction de la partie 1 : 
     # cumulative_input = [sum(input[i-window_size+1:i+1]) for i in range(2, len(input))]
-    # result = process(cumulative_input)
+    # result = solve_part_one(cumulative_input)
 
     # Version optimisée sans création de tableau intermédiaire : 
-    result = sum([1 if sum(input[i-window_size+1:i+1]) > sum(input[i-window_size:i]) else 0 for i in range(3, len(input))])
-    
+    result = solve_part_one_and_two(input, window_size=3)
     print(f'Day 01 Part Two: {result}')
 
     print("--- %.2f seconds ---" % (time.time() - start_time))
