fb843e2b78cde9eb007e19b1b7e241d08465f722
PhunkyBob <github.com@e-courrier.eu>
2021-12-08 15:55:06 +0100

    Format text

--- 2021/2021_day_07_AB.py
+++ 2021/2021_day_07_AB.py
@@ -3,11 +3,12 @@
 
 import time
 
+
 def load_input(filename):
-    return [int(x) for x in open(filename, 'r').read().strip().split(",")]
+    return [int(x) for x in open(filename, "r").read().strip().split(",")]
 
 
-def solve_part_one(input, method='constant'):
+def solve_part_one(input, method="constant"):
     move_to_cost = {}
     move_from = min(input)
     move_to = max(input)
@@ -19,31 +20,33 @@ def solve_part_one(input, method='constant'):
     # move_to_position = [k for k, v in move_to_cost.items() if v == min_cost]
     return min_cost
 
-def calc_cost(dist, method='constant'):
-    if method == 'constant':
+
+def calc_cost(dist, method="constant"):
+    if method == "constant":
         return dist
     else:
         return dist * (dist + 1) // 2
 
-def solve_part_two(input):
-    return solve_part_one(input, method='more and more expensive')
 
-if __name__ == '__main__':
+def solve_part_two(input):
+    return solve_part_one(input, method="more and more expensive")
+
+
+if __name__ == "__main__":
     start_time = time.time()
 
     # input_file = '2021_day_07_input_sample.txt'
-    input_file = '2021_day_07_input.txt'
+    input_file = "2021_day_07_input.txt"
     input = load_input(input_file)
 
     """Part One"""
     result = solve_part_one(input)
-    print(f'Day X Part One: {result}')
+    print(f"Day X Part One: {result}")
     # Your puzzle answer was 335330.
 
     """Part Two"""
     result = solve_part_two(input)
-    print(f'Day X Part Two: {result}')
+    print(f"Day X Part Two: {result}")
     # Your puzzle answer was 92439766.
 
     print("--- %.2f seconds ---" % (time.time() - start_time))
-
