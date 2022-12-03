dc5229eaeefd2288814cc98f220ba5c7b4341367
PhunkyBob <github.com@e-courrier.eu>
2022-12-03 11:22:13 +0100

    AOC template

---
+++ 2022/2022_day_xx_AB.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+""" https://adventofcode.com/2022/day/X """
+DAY = "xx"
+
+from aoc_performance import aoc_perf
+
+
+def read_input(filename: str):
+    with open(filename, "r") as f:
+        content = f.read()
+    return content
+
+
+def part_one(filename: str) -> int:
+    data = read_input(filename)
+    # Code
+    return
+
+
+def part_two(filename: str) -> int:
+    data = read_input(filename)
+    # Code
+    return
+
+
+def main() -> None:
+    input_filename = f"2022_day_{DAY}_input_sample.txt"
+    input_filename = f"2022_day_{DAY}_input.txt"
+
+    with aoc_perf():
+        print(f"Day {DAY} Part One")
+        answer = part_one(input_filename)
+        print(f"Answer: {answer}")
+
+    with aoc_perf():
+        print(f"Day {DAY} Part Two")
+        answer = part_two(input_filename)
+        print(f"Answer: {answer}")
+
+
+if __name__ == "__main__":
+    main()
