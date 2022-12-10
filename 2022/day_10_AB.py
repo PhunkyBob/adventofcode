a0c9fde93cc44032b2261d3ee72eff75fc6bcc17
PhunkyBob <github.com@e-courrier.eu>
2022-12-10 11:48:12 +0100

    Day 10 part two

--- 2022/day_10_AB.py
+++ 2022/day_10_AB.py
@@ -36,7 +36,44 @@ def get_signal_strength(filename: str) -> Tuple[int, int]:
                 if is_key_cycle(cycle):
                     yield cycle, x
             x += value_to_add
-        # yield cycle, x
+
+
+def is_print_pixel(cycle: int, x: int) -> bool:
+    return abs(cycle - 1 - x) <= 1
+
+
+def print_sprite_position(x: int) -> None:
+    print(f"Sprite position:", "".join(["#" if abs(i - x) <= 1 else "." for i in range(40)]))
+
+
+def print_sprites(filename: str, debug=False) -> None:
+    LINE_WIDTH = 40
+    x = 1
+    cycle = 0
+    with open(filename, "r") as f:
+        current_crt_row = ""
+        for line in f:
+            cycles_to_wait, value_to_add = get_instruction(line)
+            for _ in range(cycles_to_wait):
+                cycle += 1
+                pixel = "#" if is_print_pixel(cycle, x) else "."
+
+                if debug:
+                    current_crt_row += pixel
+                    print(f"During cycle  {cycle}: CRT draws pixel in position {cycle-1}")
+                    print(f"Current CRT row: {current_crt_row}")
+                else:
+                    print(pixel, end="")
+                if cycle % LINE_WIDTH == 0:
+                    cycle = 0
+                    if debug:
+                        current_crt_row = ""
+                    else:
+                        print()
+            x += value_to_add
+            if debug:
+                print(f"End of cycle  {cycle}: finish executing addx {value_to_add} (Register X is now {x})")
+                print_sprite_position(x)
 
 
 def part_one(filename: str) -> int:
@@ -46,8 +83,8 @@ def part_one(filename: str) -> int:
     return total_strength
 
 
-def part_two(filename: str) -> int:
-    # Code
+def part_two(filename: str) -> None:
+    print_sprites(filename, False)
     return
 
 
