a599e17028b29e3957b0cff5dc2a7d741b0a496b
PhunkyBob <github.com@e-courrier.eu>
2022-12-22 10:45:52 +0100

    Improve part 1 with LineString + visual

--- 2022/day_15_AB.py
+++ 2022/day_15_AB.py
@@ -52,10 +52,8 @@ DAY = "15"
 from aoc_performance import aoc_perf
 from typing import List, Dict, Tuple, Set
 import re
-from shapely.geometry import Polygon
-
-# import matplotlib.pyplot as plt
-# import geopandas as gpd
+from shapely.geometry import Polygon, LineString, MultiPolygon
+import matplotlib.pyplot as plt
 
 
 class Map:
@@ -77,31 +75,26 @@ class Map:
         self.beacons.add((beacon_x, beacon_y))
 
     def count_not_beacon_on_line(self, line_no: int) -> int:
-        items_on_line: Set = set()
+        all_lines = LineString()
         for sensor_x, sensor_y, distance in self.sensors:
             remaining_dist = distance - abs(line_no - sensor_y)
-            for i in range(remaining_dist + 1):
-                if (sensor_x + i, line_no) not in self.beacons:
-                    items_on_line.add(sensor_x + i)
-                if (sensor_x - i, line_no) not in self.beacons:
-                    items_on_line.add(sensor_x - i)
-        return len(items_on_line)
+            if remaining_dist > 0:
+                new_line = LineString([(sensor_x - remaining_dist, line_no), (sensor_x + remaining_dist, line_no)])
+                all_lines = all_lines.union(new_line)
+        return int(all_lines.length)
 
     @staticmethod
     def manhattan_distance(from_x: int, from_y: int, to_x: int, to_y: int) -> int:
         return abs(from_x - to_x) + abs(from_y - to_y)
 
 
-def part_one(filename: str) -> int:
+def part_one(filename: str, line_search: int = 10) -> int:
     map = Map(filename)
-    # answer: int = map.count_not_beacon_on_line(10)
-    answer: int = map.count_not_beacon_on_line(2000000)
+    answer: int = map.count_not_beacon_on_line(line_search)
     return answer
 
 
-def part_two(filename: str) -> int:
-    MAP_SIZE = 20
-    MAP_SIZE = 4_000_000
+def part_two(filename: str, map_size: int = 20, tuning_frequency: int = 4_000_000, plot: bool = False) -> int:
     map = Map(filename)
     polygon = Polygon()
     for sensor in map.sensors:
@@ -109,31 +102,43 @@ def part_two(filename: str) -> int:
         x, y, d = sensor
         new_polygon = Polygon([(x - d, y), (x, y + d), (x + d, y), (x, y - d)])
         polygon = polygon.union(new_polygon)
-    limited_polygon = Polygon([(0, 0), (0, MAP_SIZE), (MAP_SIZE, MAP_SIZE), (MAP_SIZE, 0)])
-    result = limited_polygon.difference(polygon).bounds
-    answer_x = int(result[0]) + 1
-    answer_y = int(result[1]) + 1
-    answer = answer_x * MAP_SIZE + answer_y
+        if plot:
+            plt.plot(*new_polygon.exterior.xy)
 
-    # p = gpd.GeoSeries(resultat)
-    # p.plot()
-    # plt.show()
-
+    map_polygon = Polygon([(0, 0), (0, map_size), (map_size, map_size), (map_size, 0)])
+    if plot:
+        plt.plot(*map_polygon.exterior.xy, color="gray")
+        plt.fill(*map_polygon.exterior.xy, color="lightgray")
+    result = map_polygon.difference(polygon)
+    if type(result) == MultiPolygon:
+        for poly in result.geoms:
+            exterior = poly.exterior.coords
+            # Search for a square.
+            if exterior[0][0] == exterior[2][0] and exterior[1][1] == exterior[3][1]:
+                result = poly
+                break
+    answer_x = int(result.exterior.coords[0][0] + result.exterior.coords[2][0]) // 2
+    answer_y = int(result.exterior.coords[1][1] + result.exterior.coords[3][1]) // 2
+    if plot:
+        plt.plot(answer_x, answer_y, "+", color="black")
+        plt.text(answer_x, answer_y, f"distress beacon\n  x: {answer_x}\n  y: {answer_y}", fontsize=14, color="black")
+        plt.show()
+    answer = answer_x * tuning_frequency + answer_y
     return answer
 
 
 def main() -> None:
-    input_filename = f"day_{DAY}_input_sample.txt"
-    input_filename = f"day_{DAY}_input.txt"
+    input_filename, line_search, map_size = f"day_{DAY}_input_sample.txt", 10, 20
+    input_filename, line_search, map_size = f"day_{DAY}_input.txt", 2_000_000, 4_000_000
 
     with aoc_perf():
         print(f"Day {DAY} Part One")
-        answer = part_one(input_filename)
+        answer = part_one(input_filename, line_search)
         print(f"Answer: {answer}")
 
     with aoc_perf():
         print(f"Day {DAY} Part Two")
-        answer = part_two(input_filename)
+        answer = part_two(input_filename, map_size, plot=True)
         print(f"Answer: {answer}")
 
 
