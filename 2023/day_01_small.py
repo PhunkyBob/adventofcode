a44d90e354e3dafac14831e6113064e76872f4f6
PhunkyBob <github.com@e-courrier.eu>
2023-12-01 15:12:04 +0100

    Improve

--- 2023/day_01_small.py
+++ 2023/day_01_small.py
@@ -14,6 +14,7 @@ MAP_DIGITS: Dict[str, str] = {
 }
 
 
+<<<<<<< HEAD
 def get_first_and_last_re_part_A(text: str) -> str:
     res = re.findall(r"\d", text)
     return res[0] + res[-1]
@@ -32,3 +33,21 @@ print(f"Answer: {answer}")
 print("Day 01 Part B")
 answer = sum(int(get_first_and_last_re_part_B(line)) for line in open(input_filename, "r"))
 print(f"Answer: {answer}")
+=======
+def get_first_and_last_re_part_A(text: str) -> int:
+    res = re.findall(r"\d", text)
+    return int(res[0] + res[-1])
+
+
+def get_first_and_last_re_part_B(text: str) -> int:
+    res = re.findall(r"(?=(\d|" + "|".join(MAP_DIGITS.keys()) + "))", text)
+    return int(MAP_DIGITS.get(res[0], res[0]) + MAP_DIGITS.get(res[-1], res[-1]))
+
+
+input_filename = "day_01_input.txt"
+answer = sum(map(get_first_and_last_re_part_A, open(input_filename, "r")))
+print(f"Day 01 Part A: {answer}")
+
+answer = sum(map(get_first_and_last_re_part_B, open(input_filename, "r")))
+print(f"Day 01 Part B: {answer}")
+>>>>>>> d1148c3 (Minimal version)
