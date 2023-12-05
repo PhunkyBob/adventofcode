990270d30a049fd360e9ac6445ab4b0d867c592b
PhunkyBob <github.com@e-courrier.eu>
2023-12-05 19:26:50 +0100

    2023 Day 05

---
+++ 2023/tests/test_day_05.py
@@ -0,0 +1,9 @@
+from day_05 import part_A, part_B
+
+
+def test_part_A():
+    assert part_A("day_05_input_sample.txt") == 35
+
+
+def test_part_B():
+    assert part_B("day_05_input_sample.txt") == 46
