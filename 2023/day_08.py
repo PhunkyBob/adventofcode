9becb3e0b1d0c2d6569b4a60406c73407080905b
PhunkyBob <github.com@e-courrier.eu>
2023-12-08 12:00:25 +0100

    2023 day 8

---
+++ 2023/day_08.py
@@ -0,0 +1,78 @@
+"""
+Advent of Code 2023
+--- Day 8: Haunted Wasteland ---
+https://adventofcode.com/2023/day/8
+
+"""
+from typing import Any, Callable, List, Dict, Tuple
+from aoc_performance import aoc_perf
+import re
+from math import gcd
+
+DAY = "08"
+
+
+def lcm_of_list(numbers):
+    def lcm(x, y):
+        return x * y // gcd(x, y)
+
+    result = 1
+    for num in numbers:
+        result = lcm(result, num)
+    return result
+
+
+def read_input(input_filename: str) -> Tuple[str, Dict[str, Dict[str, str]]]:
+    data: Dict[str, Dict[str, str]] = {}
+    with open(input_filename, "r") as input_file:
+        lines = input_file.read().splitlines()
+        directions = lines[0]
+        for line in lines[2:]:
+            if res := re.match(r"(.+) = \((.+), (.+)\)", line):
+                key, left, right = res.groups()
+                data[key] = {"L": left, "R": right}
+    return directions, data
+
+
+def part_A(input_filename: str) -> int:
+    directions, data = read_input(input_filename)
+    current = "AAA"
+    steps = 0
+    while current != "ZZZ":
+        current = data[current][directions[steps % len(directions)]]
+        steps += 1
+    return steps
+
+
+def part_B(input_filename: str) -> int:
+    directions, data = read_input(input_filename)
+    currents = [d for d in data.keys() if d[-1] == "A"]
+    steps_list = []
+    for current in currents:
+        steps = 0
+        while not current.endswith("Z"):
+            current = data[current][directions[steps % len(directions)]]
+            steps += 1
+        steps_list.append(steps)
+    return lcm_of_list(steps_list)
+
+
+def main() -> None:
+    input_filename = f"day_{DAY}_input.txt"
+    # input_filename = f"day_{DAY}_input_sample.txt"
+
+    with aoc_perf(memory=True):
+        print(f"Day {DAY} Part A")
+        answer = part_A(input_filename)
+        print(f"Answer: {answer}")
+        # Expected: 18157
+
+    with aoc_perf(memory=True):
+        print(f"Day {DAY} Part B")
+        answer = part_B(input_filename)
+        print(f"Answer: {answer}")
+        # Expected: 14299763833181
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2023/tests/test_day_08.py
@@ -0,0 +1,10 @@
+from day_08 import part_A, part_B
+
+
+def test_part_A():
+    assert part_A("day_08_input_sample.txt") == 2
+    assert part_A("day_08_input_sample2.txt") == 6
+
+
+def test_part_B():
+    assert part_B("day_08_input_sample3.txt") == 6
