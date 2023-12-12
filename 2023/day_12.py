e47ec2cd42c0d7f3befb2fd59cb1a768b5763853
PhunkyBob <github.com@e-courrier.eu>
2023-12-12 23:31:58 +0100

    2023 day 12 part B

--- 2023/day_12.py
+++ 2023/day_12.py
@@ -1,36 +1,38 @@
 """
 Advent of Code 2023
-
+--- Day 12: Hot Springs ---
 https://adventofcode.com/2023/day/12
+
+However, the condition records are partially damaged; some of the springs' conditions are actually unknown (?). For example:
+
+???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1
 
 """
 from functools import lru_cache
-from typing import Any, Callable, List, Dict, Tuple
+from typing import List, Tuple
 from aoc_performance import aoc_perf
 import re
 
 DAY = "12"
 
 
-@lru_cache
-def count_possible(input: str, pattern: str) -> int:
+def count_possible_regex(input: str, pattern: str) -> int:
     new_pattern = r"(\.|\?)+".join([r"(#|\?)" * int(i) for i in pattern.split(",")])
     new_pattern = r"^(\.|\?)*" + new_pattern + r"(\.|\?)*$"
     if re.match(new_pattern, input):
         if "?" in input:
-            return count_possible(input.replace("?", ".", 1), pattern) + count_possible(
+            return count_possible_regex(input.replace("?", ".", 1), pattern) + count_possible_regex(
                 input.replace("?", "#", 1), pattern
             )
         else:
             # print(input)
             return 1
     return 0
-
-
-def unfold(input: str, pattern: str) -> Tuple[str, str]:
-    new_input = "?".join([input] * 5)
-    new_patter = ",".join([pattern] * 5)
-    return new_input, new_patter
 
 
 def read_input(input_filename: str) -> List[Tuple[str, str]]:
@@ -40,12 +42,58 @@ def read_input(input_filename: str) -> List[Tuple[str, str]]:
 
 def part_A(input_filename: str) -> int:
     data = read_input(input_filename)
-    return sum(count_possible(d[0], d[1]) for d in data)
+    # return sum(count_possible_regex(d[0], d[1]) for d in data)
+    return sum(count_possible_part2(springs_to_tuple(d[0], False), size_to_tuple(d[1], False)) for d in data)
+
+
+def unfold(input: str, separator: str, times: int = 5) -> str:
+    return separator.join([input] * 5)
+
+
+def springs_to_tuple(sequence: str, apply_unfold: bool = False) -> Tuple[str, ...]:
+    if apply_unfold:
+        sequence = unfold(sequence, "?")
+    return tuple(elem for elem in sequence.split(".") if elem)
+
+
+def size_to_tuple(sequence: str, apply_unfold: bool = False) -> Tuple[int, ...]:
+    if apply_unfold:
+        sequence = unfold(sequence, ",")
+    return tuple(int(elem) for elem in sequence.split(",") if elem)
+
+
+@lru_cache
+def is_possible(spring: str, size: int) -> bool:
+    pattern = r"^[#|\?]{" + str(size) + "}" + r"(\?|$)"
+    return re.match(pattern, spring) is not None
+
+
+@lru_cache
+def count_possible_part2(springs: Tuple[str, ...], sizes: Tuple[int, ...]) -> int:
+    if not sizes:
+        return 1 if all(spring.replace("?", "") == "" for spring in springs) else 0
+    if not springs:
+        return 0
+
+    first_spring = springs[0]
+    if not first_spring:
+        return count_possible_part2(springs[1:], sizes)
+
+    if first_spring[0] == "#":
+        return (
+            count_possible_part2((first_spring[sizes[0] + 1 :],) + springs[1:], sizes[1:])
+            if is_possible(first_spring, sizes[0])
+            else 0
+        )
+    # else char == "?":
+    res_when_empty = count_possible_part2((first_spring[1:],) + springs[1:], sizes)
+    res_when_broken = count_possible_part2((f"#{first_spring[1:]}",) + springs[1:], sizes)
+    return res_when_empty + res_when_broken
 
 
 def part_B(input_filename: str) -> int:
     data = read_input(input_filename)
-    return 0
+    return sum(count_possible_part2(springs_to_tuple(d[0], True), size_to_tuple(d[1], True)) for d in data)
 
 
 def main() -> None:
---
+++ 2023/tests/test_day_12.py
@@ -0,0 +1,51 @@
+from day_12 import (
+    DAY,
+    part_A,
+    part_B,
+    count_possible_regex,
+    unfold_all,
+    count_possible_part2,
+    springs_to_tuple,
+    size_to_tuple,
+)
+
+
+def test_count_possible():
+    assert count_possible_regex("???.###", "1,1,3") == 1
+    assert count_possible_regex(".??..??...?##.", "1,1,3") == 4
+    assert count_possible_regex("?#?#?#?#?#?#?#?", "1,3,1,6") == 1
+    assert count_possible_regex("????.#...#...", "4,1,1") == 1
+    assert count_possible_regex("????.######..#####.", "1,6,5") == 4
+    assert count_possible_regex("?###????????", "3,2,1") == 10
+
+
+def test_unfold():
+    assert unfold_all(".#", "1") == (".#?.#?.#?.#?.#", "1,1,1,1,1")
+    assert unfold_all("???.###", "1,1,3") == (
+        "???.###????.###????.###????.###????.###",
+        "1,1,3,1,1,3,1,1,3,1,1,3,1,1,3",
+    )
+    assert unfold_all("###", "1,1,1") == ("###?###?###?###?###", "1,1,1,1,1,1,1,1,1,1,1,1,1,1,1")
+
+
+def test_count_possible_part2():
+    assert count_possible_part2(springs_to_tuple("???.###"), size_to_tuple("1,1,3")) == 1
+    assert count_possible_part2(springs_to_tuple(".??..??...?##."), size_to_tuple("1,1,3")) == 4
+    assert count_possible_part2(springs_to_tuple("?#?#?#?#?#?#?#?"), size_to_tuple("1,3,1,6")) == 1
+    assert count_possible_part2(springs_to_tuple("????.#...#..."), size_to_tuple("4,1,1")) == 1
+    assert count_possible_part2(springs_to_tuple("????.######..#####."), size_to_tuple("1,6,5")) == 4
+    assert count_possible_part2(springs_to_tuple("?###????????"), size_to_tuple("3,2,1")) == 10
+    assert count_possible_part2(springs_to_tuple("???.###", True), size_to_tuple("1,1,3", True)) == 1
+    assert count_possible_part2(springs_to_tuple(".??..??...?##.", True), size_to_tuple("1,1,3", True)) == 16384
+    assert count_possible_part2(springs_to_tuple("?#?#?#?#?#?#?#?", True), size_to_tuple("1,3,1,6", True)) == 1
+    assert count_possible_part2(springs_to_tuple("????.#...#...", True), size_to_tuple("4,1,1", True)) == 16
+    assert count_possible_part2(springs_to_tuple("????.######..#####.", True), size_to_tuple("1,6,5", True)) == 2500
+    assert count_possible_part2(springs_to_tuple("?###????????", True), size_to_tuple("3,2,1", True)) == 506250
+
+
+def test_part_A():
+    assert part_A(f"day_{DAY}_input_sample.txt") == 21
+
+
+def test_part_B():
+    assert part_B(f"day_{DAY}_input_sample.txt") == 525152
