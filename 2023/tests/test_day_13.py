7d6040f31c0e967a96a201f321a658b1ba92fe9c
PhunkyBob <github.com@e-courrier.eu>
2023-12-14 10:18:40 +0100

    Add tests

--- 2023/tests/test_day_13.py
+++ 2023/tests/test_day_13.py
@@ -1,4 +1,15 @@
-from day_13 import DAY, part_A, part_B, read_input, find_horizontal_symetry, rotate, find_vertical_symetry
+from day_13 import (
+    DAY,
+    part_A,
+    part_B,
+    read_input,
+    find_horizontal_symetry,
+    rotate,
+    find_vertical_symetry,
+    find_horizontal_symetry_block,
+    find_vertical_symetry_block,
+    modified_array_generator,
+)
 
 
 def test_find_horizontal_symetry():
@@ -11,6 +22,18 @@ def test_find_vertical_symetry():
     arrays = read_input(f"day_{DAY}_input_sample.txt")
     assert find_vertical_symetry(arrays[0]) == 5
     assert find_vertical_symetry(arrays[1]) == 0
+
+
+def test_find_horizontal_symetry_block():
+    arrays = read_input(f"day_{DAY}_input_sample.txt")
+    assert find_horizontal_symetry_block(arrays[0]) == 0
+    assert find_horizontal_symetry_block(arrays[1]) == 4
+
+
+def test_find_vertical_symetry_block():
+    arrays = read_input(f"day_{DAY}_input_sample.txt")
+    assert find_vertical_symetry_block(arrays[0]) == 5
+    assert find_vertical_symetry_block(arrays[1]) == 0
 
 
 def test_rotate():
