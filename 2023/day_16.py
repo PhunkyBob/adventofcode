15b7ee0654158941e3fc8681a94d06b149b5438a
PhunkyBob <github.com@e-courrier.eu>
2023-12-16 19:28:19 +0100

    2023 day 16 part B

--- 2023/day_16.py
+++ 2023/day_16.py
@@ -4,12 +4,13 @@ Advent of Code 2023
 https://adventofcode.com/2023/day/16
 
 """
+from collections import deque
 from enum import Enum
-from typing import Any, Callable, List, Dict, NamedTuple, Set, Tuple
+from typing import Any, Callable, Deque, List, Dict, NamedTuple, Set, Tuple
 from aoc_performance import aoc_perf
 import sys
 
-sys.setrecursionlimit(16385)
+
 DAY = "16"
 
 
@@ -28,64 +29,51 @@ class Direction(Enum):
 grid: List[List[str]] = []
 bound_x: int = 0
 bound_y: int = 0
-done: Set[Tuple[Position, Direction]] = set()
-lights: Set[Position] = set()
+
+next_direction: Dict[Tuple[str, Direction], List[Direction]] = {
+    ("/", Direction.RIGHT): [Direction.UP],
+    ("/", Direction.LEFT): [Direction.DOWN],
+    ("/", Direction.UP): [Direction.RIGHT],
+    ("/", Direction.DOWN): [Direction.LEFT],
+    ("\\", Direction.RIGHT): [Direction.DOWN],
+    ("\\", Direction.LEFT): [Direction.UP],
+    ("\\", Direction.UP): [Direction.LEFT],
+    ("\\", Direction.DOWN): [Direction.RIGHT],
+    ("-", Direction.UP): [Direction.LEFT, Direction.RIGHT],
+    ("-", Direction.DOWN): [Direction.LEFT, Direction.RIGHT],
+    ("|", Direction.LEFT): [Direction.UP, Direction.DOWN],
+    ("|", Direction.RIGHT): [Direction.UP, Direction.DOWN],
+}
 
 
 def load_input(input_filename: str) -> None:
-    global grid, bound_x, bound_y
+    global grid, bound_x, bound_y, mirrors
     with open(input_filename, "r") as input_file:
         grid = [list(line.strip()) for line in input_file.readlines()]
     bound_y = len(grid)
     bound_x = len(grid[0])
 
 
-def travel(pos: Position, direction: Direction):
-    global done, lights
-    # print(pos, direction)
-    if pos.x < 0 or pos.x >= bound_x or pos.y < 0 or pos.y >= bound_y:
-        return
-    # print(done)
-    if (pos, direction) in done:
-        return
-    done.add((pos, direction))
-    lights.add(pos)
-    if (
-        grid[pos.y][pos.x] == "."
-        or (grid[pos.y][pos.x] == "|" and direction in {Direction.UP, Direction.DOWN})
-        or (grid[pos.y][pos.x] == "-" and direction in {Direction.LEFT, Direction.RIGHT})
-    ):
-        travel(Position(pos.x + direction.value[0], pos.y + direction.value[1]), direction)
-        return
-    if grid[pos.y][pos.x] == "|":
-        travel(Position(pos.x + Direction.UP.value[0], pos.y + Direction.UP.value[1]), Direction.UP)
-        travel(Position(pos.x + Direction.DOWN.value[0], pos.y + Direction.DOWN.value[1]), Direction.DOWN)
-        return
-    if grid[pos.y][pos.x] == "-":
-        travel(Position(pos.x + Direction.LEFT.value[0], pos.y + Direction.LEFT.value[1]), Direction.LEFT)
-        travel(Position(pos.x + Direction.RIGHT.value[0], pos.y + Direction.RIGHT.value[1]), Direction.RIGHT)
-        return
-    if (grid[pos.y][pos.x] == "/" and direction == Direction.RIGHT) or (
-        grid[pos.y][pos.x] == "\\" and direction == Direction.LEFT
-    ):
-        travel(Position(pos.x + Direction.UP.value[0], pos.y + Direction.UP.value[1]), Direction.UP)
-        return
-    if (grid[pos.y][pos.x] == "/" and direction == Direction.LEFT) or (
-        grid[pos.y][pos.x] == "\\" and direction == Direction.RIGHT
-    ):
-        travel(Position(pos.x + Direction.DOWN.value[0], pos.y + Direction.DOWN.value[1]), Direction.DOWN)
-        return
-    if (grid[pos.y][pos.x] == "/" and direction == Direction.UP) or (
-        grid[pos.y][pos.x] == "\\" and direction == Direction.DOWN
-    ):
-        travel(Position(pos.x + Direction.RIGHT.value[0], pos.y + Direction.RIGHT.value[1]), Direction.RIGHT)
-        return
-    if (grid[pos.y][pos.x] == "/" and direction == Direction.DOWN) or (
-        grid[pos.y][pos.x] == "\\" and direction == Direction.UP
-    ):
-        travel(Position(pos.x + Direction.LEFT.value[0], pos.y + Direction.LEFT.value[1]), Direction.LEFT)
-        return
-    return
+def get_next_position(pos: Position, direction: Direction) -> Tuple[Position, Direction]:
+    return Position(pos.x + direction.value[0], pos.y + direction.value[1]), direction
+
+
+def count_energized(start_pos: Position, start_direction: Direction) -> int:
+    queue: List[Tuple[Position, Direction]] = [(start_pos, start_direction)]
+    energized: Set[Tuple[Position, Direction]] = set()
+
+    while queue:
+        pos, direction = queue.pop()
+        if pos.x < 0 or pos.x >= bound_x or pos.y < 0 or pos.y >= bound_y:
+            continue
+        if (pos, direction) in energized:
+            continue
+        energized.add((pos, direction))
+        queue.extend(
+            get_next_position(pos, next_dir)
+            for next_dir in next_direction.get((grid[pos.y][pos.x], direction), [direction])
+        )
+    return len({pos[0] for pos in energized})
 
 
 def display_grid(lights: Set[Position]) -> None:
@@ -100,13 +88,17 @@ def display_grid(lights: Set[Position]) -> None:
 
 def part_A(input_filename: str) -> int:
     load_input(input_filename)
-    travel(Position(0, 0), Direction.RIGHT)
-    display_grid(lights)
-    return len(lights)
+    return count_energized(Position(0, 0), Direction.RIGHT)
+    # display_grid(lights)
 
 
 def part_B(input_filename: str) -> int:
-    return 0
+    load_input(input_filename)
+    max_lights = [count_energized(Position(0, y), Direction.RIGHT) for y in range(bound_y)]
+    max_lights += [count_energized(Position(bound_x - 1, y), Direction.LEFT) for y in range(bound_y)]
+    max_lights += [count_energized(Position(x, 0), Direction.DOWN) for x in range(bound_x)]
+    max_lights += [count_energized(Position(x, bound_y - 1), Direction.UP) for x in range(bound_x)]
+    return max(max_lights)
 
 
 def main() -> None:
@@ -117,11 +109,13 @@ def main() -> None:
         print(f"Day {DAY} Part A")
         answer = part_A(input_filename)
         print(f"Answer: {answer}")
+        # Expected: 7236
 
     with aoc_perf(memory=True):
         print(f"Day {DAY} Part B")
         answer = part_B(input_filename)
         print(f"Answer: {answer}")
+        # Expected: 7521
 
 
 if __name__ == "__main__":
---
+++ 2023/tests/test_day_16.py
@@ -0,0 +1,9 @@
+from day_16 import DAY, part_A, part_B
+
+
+def test_part_A():
+    assert part_A(f"day_{DAY}_input_sample.txt") == 46
+
+
+def test_part_B():
+    assert part_B(f"day_{DAY}_input_sample.txt") == 51
