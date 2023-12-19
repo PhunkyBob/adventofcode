d431c43b5c3a02c1f093c1873a6d252ce3e86427
PhunkyBob <github.com@e-courrier.eu>
2023-12-19 23:15:48 +0100

    Template for day 20

---
+++ 2023/day_20.py
@@ -0,0 +1,37 @@
+"""
+Advent of Code 2023
+
+https://adventofcode.com/2023/day/20
+
+"""
+from typing import Any, Callable, List, Dict
+from aoc_performance import aoc_perf
+
+DAY = "20"
+
+
+def part_A(input_filename: str) -> int:
+    return 0
+
+
+def part_B(input_filename: str) -> int:
+    return 0
+
+
+def main() -> None:
+    input_filename = f"day_{DAY}_input.txt"
+    input_filename = f"day_{DAY}_input_sample.txt"
+
+    with aoc_perf(memory=True):
+        print(f"Day {DAY} Part A")
+        answer = part_A(input_filename)
+        print(f"Answer: {answer}")
+
+    with aoc_perf(memory=True):
+        print(f"Day {DAY} Part B")
+        answer = part_B(input_filename)
+        print(f"Answer: {answer}")
+
+
+if __name__ == "__main__":
+    main()
