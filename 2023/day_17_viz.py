c964648ab3c87af6f7c3055df723f9a9e3601a85
PhunkyBob <github.com@e-courrier.eu>
2024-01-03 18:44:59 +0100

    Result

--- 2023/day_17_viz.py
+++ 2023/day_17_viz.py
@@ -6,6 +6,8 @@ https://adventofcode.com/2023/day/17
 Vizualisation of the solution of day 17.
 """
 from enum import Enum
+import os
+import time
 from typing import Any, Callable, List, Dict, NamedTuple, Optional, Set, Tuple
 from aoc_performance import aoc_perf
 import heapq
@@ -43,11 +45,18 @@ COLOR_WHITE = (255, 255, 255)
 COLOR_BLACK = (0, 0, 0)
 COLOR_RED = (255, 0, 0)
 COLOR_YELLOW = (255, 255, 0)
+COLOR_GREEN = (0, 255, 0)
+
+TITLE = "AoC 2023 - Day 17 - Clumsy Crucible"
 
 
 def read_input(filename: str) -> List[List[int]]:
     with open(filename, "r") as f:
         return [list(map(int, list(line.strip()))) for line in f.readlines()]
+
+
+def manathan_distance(coord1: Coord, coord2: Coord) -> int:
+    return abs(coord1[0] - coord2[0]) + abs(coord1[1] - coord2[1])
 
 
 def find_shortest_path(
@@ -56,22 +65,24 @@ def find_shortest_path(
     min_direction_count: int = 1,
     start: Coord = (0, 0),
     end: Optional[Coord] = None,
+    display_every_x_frames: int = 50,
 ):
     if end is None:
         end = (len(matrix[0]) - 1, len(matrix) - 1)
-    candidates: List[Tuple[int, Element]] = []
+    candidates: List[Tuple[int, Element, List[Coord]]] = []
     for dir in [UP, RIGHT, DOWN, LEFT]:
-        heapq.heappush(candidates, (0, (start, dir, 1)))
+        heapq.heappush(candidates, (0, (start, dir, 1), [start]))
     certains: Set[Element] = set()
     iteration = 0
+    last_path = []
+    best_cost = 9 * len(matrix) * len(matrix[0])
     while candidates:
         iteration += 1
         if iteration % 100_000 == 0:
             print(f"Iteration {iteration}")
-        if iteration % 25 == 0:
-            pygame.display.update()
 
-        cost, current = heapq.heappop(candidates)
+        cost, current, path = heapq.heappop(candidates)
+
         if current in certains:
             continue
         certains.add(current)
@@ -83,7 +94,21 @@ def find_shortest_path(
         if new_coord[0] < 0 or new_coord[0] >= len(matrix[0]) or new_coord[1] < 0 or new_coord[1] >= len(matrix):
             continue
         new_cost = cost + matrix[new_coord[1]][new_coord[0]]
+        temp_cost = new_cost + manathan_distance(new_coord, end) * 9
+        if temp_cost < best_cost:
+            best_cost = temp_cost
+        # print(new_cost)
+        if iteration % display_every_x_frames == 0:
+            pygame.display.set_caption(f"{TITLE} - Best cost {best_cost}")
+            draw_path(last_path, COLOR_RED)
+            last_path = path[:]
+            draw_path(path, COLOR_GREEN)
+            pygame.display.update()
         if min_direction_count <= current[2] <= max_direction_count and new_coord == end:
+            pygame.display.set_caption(f"{TITLE} - Best cost {best_cost}")
+            draw_path(last_path, COLOR_RED)
+            last_path = path[:]
+            draw_path(path, COLOR_GREEN)
             pygame.display.update()
             return new_cost
         for new_direction in [UP, RIGHT, DOWN, LEFT]:
@@ -95,12 +120,20 @@ def find_shortest_path(
             ):
                 continue
             if (new_coord, new_direction, new_direction_count) not in certains:
-                heapq.heappush(candidates, (new_cost, (new_coord, new_direction, new_direction_count)))
+                heapq.heappush(
+                    candidates, (new_cost, (new_coord, new_direction, new_direction_count), path + [new_coord])
+                )
                 pygame.draw.rect(
                     WIN, COLOR_YELLOW, (new_coord[0] * FONT_SIZE, new_coord[1] * FONT_SIZE, FONT_SIZE, FONT_SIZE)
                 )
+
         # pygame.display.update()
     return -1
+
+
+def draw_path(path: List[Coord], color: Tuple[int, int, int] = COLOR_GREEN):
+    for coord in path:
+        pygame.draw.rect(WIN, color, (coord[0] * FONT_SIZE, coord[1] * FONT_SIZE, FONT_SIZE, FONT_SIZE))
 
 
 def init_window(matrix: List[List[int]]) -> None:
@@ -108,13 +141,14 @@ def init_window(matrix: List[List[int]]) -> None:
 
     # FONT_SIZE = 9
     WIDTH, HEIGHT = len(matrix[0]) * FONT_SIZE, len(matrix) * FONT_SIZE
+    os.environ["SDL_VIDEO_CENTERED"] = "1"
     WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption("AoC 2023 - Day 17 - Clumsy Crucible")
-
+    pygame.display.set_caption(f"{TITLE} - PRESS [SPACE] TO START")
     FONT = pygame.font.SysFont("Arial", FONT_SIZE)
     CLOCK = pygame.time.Clock()
     WIN.fill(COLOR_BLACK)
     draw_matrix(matrix)
+    pygame.display.update()
 
 
 def fps_counter():
@@ -156,15 +190,16 @@ def main():
     matrix = read_input(input_filename)
     init_window(matrix)
 
-    find_shortest_path(matrix)
-    # find_shortest_path(matrix, max_direction_count=10, min_direction_count=4)
-
     run = True
     while run:
         CLOCK.tick(FPS)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                find_shortest_path(matrix, display_every_x_frames=10)
+                # find_shortest_path(matrix, max_direction_count=10, min_direction_count=4, display_every_x_frames=100)
 
 
 if __name__ == "__main__":
