28c60e95b5180ec76f7d8c07b3bf9e0271fa5462
PhunkyBob <github.com@e-courrier.eu>
2024-12-04 10:13:18 +0100

    Day 04 2024 first shot

---
+++ 2024/day_04.py
@@ -0,0 +1,107 @@
+"""
+Advent of Code 2024
+
+https://adventofcode.com/2024/day/4
+
+"""
+
+from typing import Any, Callable, Dict, List
+
+from aoc_performance import aoc_perf
+
+DAY = "04"
+
+DIRECTIONS = {
+    "N": (0, 1),
+    "S": (0, -1),
+    "E": (1, 0),
+    "W": (-1, 0),
+    "NE": (1, 1),
+    "NW": (-1, 1),
+    "SE": (1, -1),
+    "SW": (-1, -1),
+}
+
+
+def read_input(input_filename: str) -> List[List[str]]:
+    with open(input_filename, "r") as file:
+        return [list(line.strip()) for line in file.readlines()]
+
+
+def is_out_of_bounds(matrix: List[List[str]], x: int, y: int) -> bool:
+    return x < 0 or y < 0 or y >= len(matrix) or x >= len(matrix[y])
+
+
+def count_xmas_from(matrix: List[List[str]], start_x: int, start_y: int) -> int:
+    xmas_count = 0
+    for direction in DIRECTIONS.values():
+        x = start_x
+        y = start_y
+        if matrix[y][x] != "X":
+            continue
+        x += direction[0]
+        y += direction[1]
+        if not is_out_of_bounds(matrix, x, y) and matrix[y][x] == "M":
+            x += direction[0]
+            y += direction[1]
+            if not is_out_of_bounds(matrix, x, y) and matrix[y][x] == "A":
+                x += direction[0]
+                y += direction[1]
+                if not is_out_of_bounds(matrix, x, y) and matrix[y][x] == "S":
+                    xmas_count += 1
+    return xmas_count
+
+
+def count_mas_in_x_from(matrix: List[List[str]], start_x: int, start_y: int) -> int:
+    xmas_count = 0
+    x = start_x
+    y = start_y
+    if matrix[y][x] != "A":
+        return 0
+
+    diagonal_up_word = f"{matrix[y - 1][x - 1]}A{matrix[y + 1][x + 1]}"
+    if diagonal_up_word in ["MAS", "SAM"]:
+        diagonal_down_word = f"{matrix[y + 1][x - 1]}A{matrix[y - 1][x + 1]}"
+        if diagonal_down_word in ["MAS", "SAM"]:
+            xmas_count += 1
+
+    return xmas_count
+
+
+def part_A(input_filename: str) -> int:
+    matrix = read_input(input_filename)
+    xmas_count = 0
+    for y in range(len(matrix)):
+        for x in range(len(matrix[y])):
+            if matrix[y][x] == "X":
+                xmas_count += count_xmas_from(matrix, x, y)
+    return xmas_count
+
+
+def part_B(input_filename: str) -> int:
+    matrix = read_input(input_filename)
+    xmas_count = 0
+    for y in range(1, len(matrix) - 1):
+        for x in range(1, len(matrix[y]) - 1):
+            if matrix[y][x] == "A":
+                xmas_count += count_mas_in_x_from(matrix, x, y)
+    return xmas_count
+
+
+def main() -> None:
+    input_filename = f"day_{DAY}_input_sample.txt"
+    input_filename = f"day_{DAY}_input.txt"
+
+    with aoc_perf(memory=True):
+        print(f"Day {DAY} Part A")
+        answer = part_A(input_filename)
+        print(f"Answer: {answer}")
+
+    with aoc_perf(memory=True):
+        print(f"Day {DAY} Part B")
+        answer = part_B(input_filename)
+        print(f"Answer: {answer}")
+
+
+if __name__ == "__main__":
+    main()
